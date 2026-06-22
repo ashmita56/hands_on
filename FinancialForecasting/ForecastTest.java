2229aecd75ff4915d6d28f68d09768821cae2475
ashmita56 <ashmitaakunuru@gmail.com>
2026-06-22 18:26:44 +0530

    Add files via upload

---
+++ FinancialForecasting/ForecastTest.java
@@ -0,0 +1,19 @@
+public class ForecastTest {
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+        double growthRate = 0.10;
+        int years = 5;
+
+        double result = FinancialForecast.futureValue(
+                currentValue,
+                growthRate,
+                years
+        );
+
+        System.out.printf("Future Value after %d years = %.2f%n",
+                years,
+                result);
+    }
+}
