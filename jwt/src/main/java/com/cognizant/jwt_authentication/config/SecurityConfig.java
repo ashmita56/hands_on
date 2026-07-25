4e02f31aeccada4144d1f81c0bbcff4cc833f54b
ashmita56 <ashmitaakunuru@gmail.com>
2026-07-25 11:02:37 +0530

    Add files via upload

---
+++ jwt/src/main/java/com/cognizant/jwt_authentication/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.cognizant.jwt_authentication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .logout(logout -> logout.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll());
+
+        return http.build();
+    }
+}
